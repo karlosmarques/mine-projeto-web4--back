c1c690a3bb9dda387ea3e1529dae8b49b265d69f
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-09-16 09:32:03 -0300

    alterção no import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AuthService } from './auth/auth.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { MoviesModule } from './movies/movies.module';
 
 
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, MoviesModule],
   controllers: [],
   providers: [AuthService, PrismaService, UsersService],
 })
