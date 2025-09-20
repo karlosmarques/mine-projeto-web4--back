51137739a34f8ce895e176386a2bfee62161b4cd
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-09-20 10:30:54 -0300

    auth guard adicionado nas rotas

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param,UseGuards } from '@nestjs/common';
 import { MoviesService } from './movies.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('movies')
 export class MoviesController {
 
     constructor(private moviesService: MoviesService) {}
-
+    @UseGuards(AuthGuard)   
     @Get()
     async findAll(){
         return await this.moviesService.findAll();
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     async findOne(@Param('id') id:string){
         return await this.moviesService.findOne(+id);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch,UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/users';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private usersService: UsersService){}
   
+    @UseGuards(AuthGuard)
     @Get(':id')
     async findOne(@Param('id') id: string){
         return await this.usersService.findOne(+id);
-
     }
+
+    @UseGuards(AuthGuard)
     @Patch(':id')
     async update(@Param('id')id:string,@Body() Body:UpdateUserDto){
         return await this.usersService.update(+id,Body);
