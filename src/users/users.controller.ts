e7a2b1fc0188a93ba810bafe42b1fb7f3a4ed489
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-09-29 20:53:54 -0300

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch,UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch,UseGuards,Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/users';
 import { AuthGuard } from 'src/auth/auth.guard';
