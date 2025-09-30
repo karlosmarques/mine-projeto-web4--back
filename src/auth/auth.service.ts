dcce4f832a6abc940904c6b60e8c3e6b95127750
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-09-30 09:41:58 -0300

    logica de redefinição de senha

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,13 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { LoginDto, RegisterDto } from './dtos/auth';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-
+import { MailService } from 'src/mail/mail.service';
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma: PrismaService, private jwtService: JwtService){}
+    constructor(private prisma: PrismaService, private jwtService: JwtService,  private mailService: MailService,){}
 
 
      async register(data:RegisterDto){
@@ -51,4 +51,49 @@ export class AuthService {
         
          return {token};
      }
+
+     async forgotPassword(email: string) {
+    // Verifica se existe usuário com esse email
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    // Gera token JWT válido por 15 minutos
+    const token = this.jwtService.sign(
+      { userId: user.id },
+      { secret: process.env.JWT_SECRET, expiresIn: '15m' },
+    );
+
+    // Envia email para o email digitado pelo usuário
+    await this.mailService.sendPasswordReset(user.email, token);
+
+    return { message: 'Email de recuperação enviado!' };
+  }
+
+  async resetPassword(token: string, newPassword: string) {
+    try {
+      // Verifica token
+      const payload = this.jwtService.verify(token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      // Criptografa nova senha
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+      // Atualiza no banco
+      await this.prisma.user.update({
+        where: { id: payload.userId },
+        data: { password: hashedPassword },
+      });
+
+      return { message: 'Senha atualizada com sucesso!' };
+    } catch (error) {
+      throw new UnauthorizedException('Token inválido ou expirado');
+    }
+  }
+
 }
